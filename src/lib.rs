6e9608bd7d05f0728d76cde28531db1318413112
Michael Richardson <mcr@sandelman.ca>
2019-08-19 20:57:09 -0400

    first horrible hack learning Rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,41 @@
+extern crate base64;
+
+
 #[cfg(test)]
 mod tests {
+    //  use std::io::prelude::*;
+    use std::fs::File;
+    use std::io::Read;
+    // use std::u8;
+    // use self::base64::{decode};
+    // use base64::{encode, decode};
+    use base64::decode;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn it_loads_cbor_1() {
+        /* open the file */
+        let mut cborin = File::open("data/voucher_00-d0-e5-02-00-2d.b64").unwrap();
+        let mut buffer = Vec::new();
+
+        // read the whole file
+        cborin.read_to_end(&mut buffer).unwrap();
+        
+        println!("buffer1 => {} {:x?}", buffer.len(), buffer);
+        // remove whitespace
+        buffer.retain(|b| !b" \n\t\r\x0b\x0c".contains(b));
+        
+        println!("buffer2 => {} {:x?}", buffer.len(), buffer);
+        
+        let result = &decode(&mut buffer);
+        println!("buffer => {:x?}", buffer);
+        match result {
+            Ok(data) => {
+                println!("read => {:x?}", data);
+            }
+            Err(err) => {
+                println!("error: {:?}", err);
+            }
+        }
     }
 }
+
