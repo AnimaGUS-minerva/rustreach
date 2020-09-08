4a25056b205fae754f37c40d163a2e988b3fce15
Michael Richardson <mcr@sandelman.ca>
2020-09-07 20:42:17 -0400

    create new/fresh test data from fixed chariwt, and reach/fountain/highway trio

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,10 @@ mod tests {
     // use std::u8;
     // use self::base64::{decode};
     use base64;
+    use cose;
+
+    //fn voucher_load(buffer: Vec<u8>) -> voucher {
+    //}
 
     #[test]
     fn it_loads_cbor_1() {
@@ -21,14 +25,32 @@ mod tests {
         // read the whole file
         cborin.read_to_end(&mut buffer).unwrap();
         
-        println!("buffer1 => {} {:x?}", buffer.len(), buffer);
+        //println!("buffer1 => {} {:x?}", buffer.len(), buffer);
         // remove whitespace
         buffer.retain(|b| !b" \n\t\r\x0b\x0c".contains(b));
         
-        println!("buffer2 => {} {:x?}", buffer.len(), buffer);
+        //println!("buffer2 => {} {:x?}", buffer.len(), buffer);
         
         let voucher_bytes = &base64::decode(&mut buffer).unwrap();
-        println!("voucher_bytes => {:x?}", voucher_bytes);
+        //println!("voucher_bytes => {:x?}", voucher_bytes);
+
+        let result = cose::decoder::decode_signed_object(&voucher_bytes, None);
+        assert!(result.is_err());
+
+        let mut coseobjs = result.unwrap();
+        assert_eq!(coseobjs.len(), 1); // Vec of length 1.
+        let firstsig = coseobjs.pop().unwrap();
+
+        assert_eq!(firstsig.signature_type, cose::SignatureAlgorithm::ES256);
+
+        //Ok(CoseSignature {
+        //signature_type: signature_algorithm,
+        //signature: signature_bytes,
+        //signer_cert: ee_cert,
+        //certs: certs,
+        //to_verify: sig_structure_bytes,
+        //})
+        
     }
 }
 
